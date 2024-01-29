a9aa71fde9ad68483759bce6e15ea5df73df7413
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-29 10:38:59 -0300

    session3, lecture3, strings

---
+++ lecture3.py
@@ -0,0 +1,18 @@
+# Strings
+
+"""
+DocString - Document your code
+
+"""
+
+# String with double quotes
+print("Texto 'entre aspas'")
+
+# String with single quotes
+print('Texto "entre aspas"')
+
+# String with escape
+print("Texto \"entre aspas\"")
+
+# String with r, for regular expressions
+print(r"Texto \"entre aspas\"")
