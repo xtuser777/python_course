5c884cd02844e8987ed73059894b38354c87dc4f
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-29 10:56:00 -0300

    session3, lecture4, numbers

---
+++ lecture4.py
@@ -0,0 +1,13 @@
+# Numbers
+
+# int - integer number, negative or positive
+
+integer = 123
+
+print(type(integer), integer)
+
+# float - numbers with float point for decimal numbers, negativer or positive
+
+float_point = 123.23
+
+print(type(float_point), float_point)
