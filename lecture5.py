6f5ba0d005d257daf6bb92e82e3816736a51a582
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-29 11:05:39 -0300

    session3, lecture5, booleans

---
+++ lecture5.py
@@ -0,0 +1,10 @@
+# Boolean
+
+# bool - for logic values True or False, result of a question of code about values or relations entire values with logic operators
+bool1 = True
+bool2 = False
+bool3 = 1 == 1
+bool4 = 1 > 0
+
+print(bool1, bool2, bool3, bool4)
+print(type(bool1), type(bool2), type(bool3), type(bool4))
