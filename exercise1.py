d0f60cff08fb23914bcbb52d8fd8fb8d76397782
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-29 17:43:46 -0300

    session3, exercise1,  variables and types

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+name = 'Lucas'
+second_name = 'Oliveira'
+age = 29
+birth_year = 1994
+adult = age >= 18
+height = 1.94
+
+print('Nome:', name)
+print('Sobrenome:', second_name)
+print('Idade:', age)
+print('Ano de nascimento:', birth_year)
+print('Maior de idade?', adult)
+print('Altura em metros:', height)
