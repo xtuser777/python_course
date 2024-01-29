90ee8034891681a0d00a65da68c204a97c3aa722
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-29 10:25:19 -0300

    session3, lecture1, comments

---
+++ lecture1.py
@@ -0,0 +1,12 @@
+# Line comment
+print(123)
+
+"""
+    DocString with double quotes
+    Possibility multi line comment, but it is not a comment
+"""
+
+'''
+    DocString with single quotes
+    Possibility multi line comment, but it is not a comment
+'''
