f0437613f60288576d6be5914caaab730399e8ef
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-29 17:27:36 -0300

    session3, lecture 7, variables

---
+++ lecture7.py
@@ -0,0 +1,14 @@
+# PEP8: inicie variáveis com letras minúsculas, pode usar números e underline _.
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Luiz Otávio Miranda'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+name = 'Luiz'
+age = 17
+adult = age >= 18
+print('name:', name, 'age:', age)
+print('É maior?', adult)
