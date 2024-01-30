184a0f15c688f9355d9253d9a4bb0fd801c5b49c
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-30 08:49:43 -0300

    session3, lecture9, math operations

---
+++ lecture9.py
@@ -0,0 +1,26 @@
+addition = 10 + 10
+print('Adição', addition)
+
+subtraction = 10 - 5
+print('Subtração', subtraction)
+
+multiplication = 10 * 10
+print('Multiplicação', multiplication)
+
+division = 10 / 3  # float
+print('Divisão', division)
+
+integer_division = 10 // 3
+print('Divisão inteira', integer_division)
+
+exponentiation = 2 ** 10
+print('Exponenciação', exponentiation)
+
+module = 55 % 2  # resto da divisão
+print('Módulo', module)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
