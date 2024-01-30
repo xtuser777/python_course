f781b8116273517435f6a99cf2ff617333ff1096
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-30 08:38:02 -0300

    session3, lecture8, solution of exercise1

---
+++ lecture8.py
@@ -0,0 +1,13 @@
+name = 'Luiz Otávio'
+second_name = 'Miranda'
+age = 18
+birth_year = 2022 - age
+adult = age >= 18
+meters_height = 1.80
+
+print('Nome:', name)
+print('Sobrenome:', second_name)
+print('Idade:', age)
+print('Ano de nascimento:', birth_year)
+print('É maior de idade?', adult)
+print('Altura em metros:', meters_height)
