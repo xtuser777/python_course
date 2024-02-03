3248c867d38417e3ee8dc9715e3037a22c90b56e
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-03 18:58:07 -0300

    session3, lecture17, condition flux

---
+++ lecture17.py
@@ -0,0 +1,24 @@
+# if / elif      / else
+# se / se não se / se não
+
+condiction1 = True
+condiction2 = True
+condiction3 = True
+condiction4 = True
+
+if condiction1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condiction2:
+    print('Código para condição 2')
+elif condiction3:
+    print('Código para condição 3')
+elif condiction4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
