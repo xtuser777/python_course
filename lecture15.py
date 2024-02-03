f3092f10edf72619c071740fdd6f5bc37187833f
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-03 18:24:26 -0300

    session3, lecture15, input

---
+++ lecture15.py
@@ -0,0 +1,10 @@
+# name = input('Qual o seu nome? ')
+# print(f'O seu nome é {name}')
+
+number_1 = input('Digite um número: ')
+number_2 = input('Digite outro número: ')
+
+int_number_1 = int(number_1)
+int_number_2 = int(number_2)
+
+print(f'A soma dos números é: {int_number_1 + int_number_2}')
