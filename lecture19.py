6981b01bf73b20ac646dddfa7e6fa558d757bb51
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-03 19:12:43 -0300

    session3, lecture19, relational operators

---
+++ lecture19.py
@@ -0,0 +1,17 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+bigger = 2 > 1
+bigger_or_equal = 2 >= 2
+less = 1 < 2
+less_or_equal = 2 <= 2
+equal = 'a' == 'a'
+diferent = 'a' != 'b'
+print('Olha meu print aqui')
