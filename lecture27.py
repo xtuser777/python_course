798d8938fed3cfcbb9a5df75a85b014edaf87f9a
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-08 11:19:35 -0300

    session3, lecture27, strings spliting and len function

---
+++ lecture27.py
@@ -0,0 +1,11 @@
+"""
+Spliting of strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variable = 'Olá mundo'
+print(variable[::-1])
